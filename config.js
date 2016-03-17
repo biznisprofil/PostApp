ee33daa1f7289212544f01d3d09a77a8544cd4ba
Boris <biznis.profil@hotmail.com>
2016-03-17 16:55:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "database": "mongodb://root:abc123@ds015289.mlab.com:15289/userstory",
+    "database": "mongodb://user:pass@ds014569.mlab.com:15289/userstory",
     "port": process.env.PORT || 3000,
     "secretKey": "YourSecretKey"
 }
